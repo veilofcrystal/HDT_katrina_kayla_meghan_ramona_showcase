0a5a7f8a11279148478da7e975e2f01f455c8de4
Ramona <144703303+Ramona-Lozon@users.noreply.github.com>
2024-10-14 14:08:18 -0400

    updated schedule interactivity to a working state

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const navLinks = document.querySelector('#navLinks');
 const menuIcon = document.querySelector('#menuIcon');
+const whenContent = document.getElementById('whenContent');
+const whereContent = document.getElementById('whereContent');
+const scheduleOne = document.getElementById('scheduleOne');
+const scheduleTwo = document.getElementById('scheduleTwo');
+const schduleThree = document.getElementById('scheduleThree');
 const player = new Plyr('video'); 
 
 function toggleMenu() {
@@ -7,5 +12,50 @@ function toggleMenu() {
     navLinks.classList.toggle('active');
 }
 
-menuIcon.addEventListener('click', toggleMenu);
+function toggleWhen() {
+    console.log("when clicked")
+    if (whenContent.classList.contains("hidden")) {
+        whenContent.classList.remove("hidden");
+    } else {
+        whenContent.classList.add("hidden");
+}}
 
+function toggleWhere() {
+    console.log("where clicked")
+    if (whereContent.classList.contains("hidden")) {
+        whereContent.classList.remove("hidden");
+    } else {
+        whereContent.classList.add("hidden");
+}}
+
+function toggleSchedOne() {
+    console.log("Schedule one clicked")
+    if (scheduleOne.classList.contains("hidden")) {
+        scheduleOne.classList.remove("hidden");
+    } else {
+        scheduleOne.classList.add("hidden");
+}}
+
+function toggleSchedTwo() {
+    console.log("Schedule Two clicked")
+    if (scheduleTwo.classList.contains("hidden")) {
+        scheduleTwo.classList.remove("hidden");
+    } else {
+        scheduleTwo.classList.add("hidden");
+}}
+
+
+function toggleSchedThree() {
+    console.log("Schedule three clicked")
+    if (scheduleThree.classList.contains("hidden")) {
+        scheduleThree.classList.remove("hidden");
+    } else {
+        scheduleThree.classList.add("hidden");
+}}
+
+menuIcon.addEventListener('click', toggleMenu);
+whenPanel.addEventListener('click', toggleWhen);
+wherePanel.addEventListener('click', toggleWhere);
+schedOnePanel.addEventListener('click', toggleSchedOne);
+schedTwoPanel.addEventListener('click', toggleSchedTwo);
+schedThreePanel.addEventListener('click', toggleSchedThree);
